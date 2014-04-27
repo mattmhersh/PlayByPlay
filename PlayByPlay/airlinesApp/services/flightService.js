d38ede625b2892ea98588ea793442d9a0e5b5074
matthersh <matt.hersh@ascensus.com>
2014-04-27 17:03:38 -0400

    Prefixed all ng tags with 'data' so HTML standard knows to ignore tag. Will be used by Angularjs.

--- PlayByPlay/airlinesApp/services/flightService.js
+++ PlayByPlay/airlinesApp/services/flightService.js
@@ -1,31 +1,36 @@
 ﻿(function(app) {
     var flightService = function() {
+        var testData = [
+            {
+                airline: "United Airlines",
+                flightNumber: 207,
+                startTime: "13:00",
+                endTime: "15:00",
+                totalDuration: 2,
+                price: 500,
+                date: "12/1/2014",
+                cities: ["ORD", "LAS"],
+                approved: false
+            },
+            {
+                airline: "American Airlines",
+                flightNumber: 207,
+                startTime: "13:00",
+                endTime: "15:00",
+                totalDuration: 2,
+                price: 500,
+                date: "12/1/2014",
+                cities: ["SFO", "LAS"],
+                approved: false
+            }
+        ];
+
         var flightFactory = {};
         flightFactory.getFlights = function() {
-            return [
-                    {
-                        airline: "United Airlines",
-                        flightNumber: 207,
-                        startTime: "13:00",
-                        endTime: "15:00",
-                        totalDuration: 2,
-                        price: 500,
-                        date: "12/1/2014",
-                        cities: ["ORD", "LAS"],
-                        approved: false
-                    },
-                    {
-                        airline: "American Airlines",
-                        flightNumber: 207,
-                        startTime: "13:00",
-                        endTime: "15:00",
-                        totalDuration: 2,
-                        price: 500,
-                        date: "12/1/2014",
-                        cities: ["SFO", "LAS"],
-                        approved: false
-                    }
-            ];
+            return testData;
+        };
+        flightFactory.selectFlight = function() {
+
         };
         return flightFactory;
     };
