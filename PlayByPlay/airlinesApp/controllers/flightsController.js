7ca2ae8c2fecb65b84bdffcfb1cf3e1d3165ea63
matthersh <matt.hersh@ascensus.com>
2014-04-27 15:04:45 -0400

    Added approve button

--- PlayByPlay/airlinesApp/controllers/flightsController.js
+++ PlayByPlay/airlinesApp/controllers/flightsController.js
@@ -4,8 +4,28 @@
 
         $scope.flights =
                 [
-                    { airline: "United", flightNumber: 207, startTime: "13:00", endTime: "15:00", totalDuration: 2, price: 500, date: "12/1/2014", cities: ["ORD","LAS"] },
-                    { airline: "United", flightNumber: 207, startTime: "13:00", endTime: "15:00", totalDuration: 2, price: 500, date: "12/1/2014", cities: ["SFO", "LAS"] }
+                    {
+                        airline: "United Airlines",
+                        flightNumber: 207,
+                        startTime: "13:00",
+                        endTime: "15:00",
+                        totalDuration: 2,
+                        price: 500,
+                        date: "12/1/2014",
+                        cities: ["ORD", "LAS"],
+                        approved: false
+                    },
+                    {
+                        airline: "American Airlines",
+                        flightNumber: 207,
+                        startTime: "13:00",
+                        endTime: "15:00",
+                        totalDuration: 2,
+                        price: 500,
+                        date: "12/1/2014",
+                        cities: ["SFO", "LAS"],
+                        approved: false
+                    }
                 ];
 
     };
